ff4ae2d8207ecd21bc3048970a4fabd4fa3e8b25
univwang <2162768982@qq.com>
2023-11-17 23:42:38 +0800

    解决缓存穿透问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -17,4 +29,48 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryById(Long id) {
+        String key = CACHE_SHOP_KEY + id;
+        //1.从redis查询缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        //2.如果缓存中存在数据，直接返回
+        if(StrUtil.isNotBlank(shopJson)) {
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+
+        if(shopJson != null ) {
+            //不是null，说明缓存中存在空值，直接返回
+            return Result.fail("店铺不存在");
+        }
+        //3.如果缓存中不存在数据，从数据库查询
+        Shop shop = getById(id);
+        //4.不存在数据，返回错误信息
+        if(shop == null) {
+            //将空值写入缓存，防止缓存穿透
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return Result.fail("店铺不存在");
+        }
+        //5.存在数据，写入缓存
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        //6.返回数据
+        return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        if(shop.getId() == null) {
+            return Result.fail("店铺id不能为空");
+        }
+        //1. 更新数据库
+        updateById(shop);
+        //2. 删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + shop.getId());
+        //3. 返回结果
+        return Result.ok();
+    }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -17,4 +25,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryTypeList() {
+        //1. 查询redis缓存
+        String cache = stringRedisTemplate.opsForValue().get("cache:shop:shop-type-list");
+        //删除cache的第一个和最后一个字符
+        //2. 如果缓存中存在数据，直接返回
+        if(cache != null) {
+            List<ShopType> list = JSONUtil.toList(JSONUtil.parseArray(cache), ShopType.class);
+            return Result.ok(list);
+        }
+        //3. 如果缓存中不存在数据，从数据库查询
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+        //4. 不存在数据，返回错误信息
+        if(typeList == null || typeList.isEmpty()) {
+            return Result.fail("店铺类型不存在");
+        }
+        //5. 存在数据，写入缓存
+        stringRedisTemplate.opsForValue().set("cache:shop:shop-type-list", JSONUtil.toJsonStr(typeList));
+        stringRedisTemplate.expire("cache:shop:shop-type-list", 60 * 24, TimeUnit.MINUTES);
+        //6. 返回数据
+        return Result.ok(typeList);
+    }
 }
